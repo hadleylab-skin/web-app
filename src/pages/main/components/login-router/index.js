9efd064522485366e8d4a5b2998c51927b3f683b
Ilya Beda <ir4y.ix@gmail.com>
2017-09-15 15:25:09 +0300

    Fix #3

--- src/pages/main/components/login-router/index.js
+++ src/pages/main/components/login-router/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import BaobabPropTypes from 'baobab-prop-types';
 import _ from 'lodash';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Image, Grid } from 'semantic-ui-react';
 import schema from 'libs/state';
 import { ServiceProvider } from 'components';
 import { getRacesList } from 'services/constants';
 import { BaseLayout } from './layout';
 import { LoginForm } from './login-form';
-
 import { AppRouter } from '../app-router';
+import step1 from './assets/step1.png';
+import step2 from './assets/step2.png';
+import step3 from './assets/step3.png';
 
 const model = {
     tree: {
@@ -67,10 +69,32 @@ export const LoginRouter = schema(model)(React.createClass({
         if (_.isEmpty(privateKey)) {
             return (
                 <BaseLayout>
-                    <Container text>
-                        <Header as="h1">You don't export your private key yet</Header>
-                        <p>Please follow the instruction to export your private key for the web app.</p>
-                    </Container>
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column width={16}>
+                                <Container text>
+                                    <Header as="h1">You don't export your private key yet</Header>
+                                    <p>Please follow the instruction to export your private key from the iOS app.</p>
+                                    <p>You need to log into the iOS app first.</p>
+                                </Container>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={2} />
+                            <Grid.Column width={6}>
+                                <p>Open 'My Profile' tab </p>
+                                <Image size="medium" src={step1} />
+                            </Grid.Column>
+                            <Grid.Column width={6}>
+                                <p>Tap on 'Cryptography configuration'</p>
+                                <p>And enable the switch</p>
+                                <Image size="medium" src={step2} />
+                                <p>If you see this you key is successfully exported</p>
+                                <Image size="medium" src={step3} />
+                            </Grid.Column>
+                            <Grid.Column width={2} />
+                        </Grid.Row>
+                    </Grid>
                 </BaseLayout>
             );
         }
