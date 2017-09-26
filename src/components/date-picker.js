8edcc60e402ac941276cf5184618c6ed2647a68f
Ilya Beda <ir4y.ix@gmail.com>
2017-09-26 15:48:51 +0300

    Fix #17

--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -3,12 +3,25 @@ import BaobabPropTypes from 'baobab-prop-types';
 import DatePickerDefault from 'react-datepicker';
 import moment from 'moment';
 
+const dateRe = new RegExp('^\\d{2}\/\\d{2}\/\\d{4}');
+
 export function DatePicker({ cursor, ...props }) {
     const date = moment(cursor.get());
+    const selected = date.isValid() ? date : moment();
+    const onChange = (newDate) => cursor.set(newDate ? newDate.format() : null);
+    const onChangeRaw = (e) => {
+        const value = e.target.value;
+        if (dateRe.exec(value) != null) {
+            cursor.set(value);
+        } else {
+            e.target.value = selected.format('MM/DD/YYYY');
+        }
+    };
     return (
         <DatePickerDefault
-            selected={date.isValid() ? date : moment()}
-            onChange={(newDate) => cursor.set(newDate.format())}
+            selected={selected}
+            onChange={onChange}
+            onBlur={onChangeRaw}
             {...props}
         />
     );
