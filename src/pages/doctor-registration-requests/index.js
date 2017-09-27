7d77f786bb290dd3994255d3704c1ebf6aef4cc2
Ilya Beda <ir4y.ix@gmail.com>
2017-09-27 12:23:33 +0300

    Fix #5 Implement Doctors registration requests page

--- src/pages/doctor-registration-requests/index.js
+++ src/pages/doctor-registration-requests/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import _ from 'lodash';
 import BaobabPropTypes from 'baobab-prop-types';
 import { GridWrapper } from 'components';
 import schema from 'libs/state';
-import { Grid, Header, Button } from 'semantic-ui-react';
+import { Grid, Header, Table, Label, Button, Message } from 'semantic-ui-react';
 
 export const DoctorResistrationRequestsPage = React.createClass({
     // We need this wrapper to make hot module replacement work
@@ -11,18 +12,144 @@ export const DoctorResistrationRequestsPage = React.createClass({
     },
 });
 
-const model = {
+const model = (props, context) => ({
     tree: {
+        requests: context.services.getDoctorResistrationRequestsService,
     },
-};
+});
 
 const DoctorResistrationRequests = schema(model)(React.createClass({
     propTypes: {
         tree: BaobabPropTypes.cursor.isRequired,
     },
-    render() {
+
+    contextTypes: {
+        services: React.PropTypes.shape({
+            getDoctorResistrationRequestsService: React.PropTypes.func.isRequired,
+            handleDoctorRegistrationRequestService: React.PropTypes.func.isRequired,
+        }),
+    },
+
+
+    isLoading(pk) {
+        const status = this.props.tree.requests.data.select(pk).get('status');
+        return status === 'Loading' || typeof status === 'undefined';
+    },
+
+    async handle(pk, action, succesText) {
+        const cursor = this.props.tree.requests.data.select(pk);
+        const result = await this.context.services.handleDoctorRegistrationRequestService(
+            cursor, pk, action);
+        if (result.status === 'Succeed') {
+            cursor.message.set(succesText);
+            setTimeout(() => cursor.unset(), 5000);
+        }
+    },
+
+    async approve(pk) {
+        await this.handle(pk, 'approve', 'The request was accepted');
+    },
+
+    async reject(pk) {
+        await this.handle(pk, 'reject', 'The request was rejected');
+    },
+
+    renderTable(data) {
+        if (_.keys(data).length === 0) {
+            return (
+                <Grid.Column width={5}>
+                    <Message>
+                        There are no pending reqistration requests
+                    </Message>
+                </Grid.Column>
+            );
+        }
         return (
-            <Header>Registration requests</Header>
+            <Grid.Column>
+                <Table celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>First Name</Table.HeaderCell>
+                            <Table.HeaderCell>Last Name</Table.HeaderCell>
+                            <Table.HeaderCell>Email</Table.HeaderCell>
+                            <Table.HeaderCell>Email Is Confirmed</Table.HeaderCell>
+                            <Table.HeaderCell>Actions</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {_.map(data, (request, pk) => (
+                            <Table.Row
+                                key={pk}
+                            >
+                                <Table.Cell>{request.data.firstName}</Table.Cell>
+                                <Table.Cell>{request.data.lastName}</Table.Cell>
+                                <Table.Cell>{request.data.email}</Table.Cell>
+                                <Table.Cell>
+                                    {
+                                    request.data.isActive
+                                    ?
+                                        <Label color="green">Yes</Label>
+                                    :
+                                        <Label color="red">No</Label>
+                                    }
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {
+                                    request.message
+                                    ?
+                                        <Message positive>
+                                            {request.message}
+                                        </Message>
+                                    :
+                                        <div>
+                                            <Button
+                                                loading={this.isLoading(pk)}
+                                                onClick={() => this.approve(pk)}
+                                                color="green"
+                                            >
+                                                Approve
+                                            </Button>
+                                            <Button
+                                                loading={this.isLoading(pk)}
+                                                onClick={() => this.reject(pk)}
+                                                color="red"
+                                            >
+                                                Reject
+                                            </Button>
+                                        </div>
+                                    }
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+            </Grid.Column>
+        );
+    },
+
+    render() {
+        const requests = this.props.tree.requests.get();
+        if (requests.status !== 'Succeed') {
+            return (
+                <div>
+                    {JSON.stringify(this.props.tree.patients.get())}
+                </div>
+            );
+        }
+        return (
+            <GridWrapper>
+                <Grid>
+                    <Grid.Row />
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header>Doctors Registration Requests</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        {this.renderTable(requests.data)}
+                    </Grid.Row>
+                </Grid>
+            </GridWrapper>
         );
     },
 }));
