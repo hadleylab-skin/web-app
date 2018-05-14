62c458d86096d539eb74c6587244b14147c1a126
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-05-14 18:08:45 +0700

    Load keys for doctor in web UI

--- src/pages/patient/index.js
+++ src/pages/patient/index.js
@@ -105,29 +105,27 @@ const Patient = schema(model)(React.createClass({
     async setupDoctorKeys() {
         const { cursors, services } = this.context;
         const { doctors } = this.props.patientCursor.get('data');
-        const { pk, isCoordinator, myDoctorsPublicKeys } = cursors.doctor.data.get();
+        const { pk, myDoctorsPublicKeys } = cursors.doctor.data.get();
 
-        if (isCoordinator) {
-            const doctorsWithKeys = _.map(_.keys(myDoctorsPublicKeys), (key) => parseInt(key, 10));
-            const doctorsWithoutKeys = _.filter(
-                doctors,
-                (doctor) => !_.includes(doctorsWithKeys, doctor) && doctor !== pk
+        const doctorsWithKeys = _.map(_.keys(myDoctorsPublicKeys), (key) => parseInt(key, 10));
+        const doctorsWithoutKeys = _.filter(
+            doctors,
+            (doctor) => !_.includes(doctorsWithKeys, doctor) && doctor !== pk
+        );
+
+        if (!_.isEmpty(doctorsWithoutKeys)) {
+            const result = await services.getDoctorKeyListService(
+                this.props.tree.doctorKeys,
+                doctorsWithoutKeys
             );
-
-            if (!_.isEmpty(doctorsWithoutKeys)) {
-                const result = await services.getDoctorKeyListService(
-                    this.props.tree.doctorKeys,
-                    doctorsWithoutKeys
-                );
-                if (result.status === 'Succeed') {
-                    let keys = _.map(result.data, (item) => {
-                        let result2 = {};
-                        result2[item.pk] = item.publicKey;
-                        return result2;
-                    });
-                    keys = Object.assign({}, ...keys);
-                    cursors.doctor.data.myDoctorsPublicKeys.merge(keys);
-                }
+            if (result.status === 'Succeed') {
+                let keys = _.map(result.data, (item) => {
+                    let result2 = {};
+                    result2[item.pk] = item.publicKey;
+                    return result2;
+                });
+                keys = Object.assign({}, ...keys);
+                cursors.doctor.data.myDoctorsPublicKeys.merge(keys);
             }
         }
     },
