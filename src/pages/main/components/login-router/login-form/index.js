daf68bf5e02c09affac67db0879f8083ee5b4596
Alexandr Polev <aleksandr.p@pieoneers.com>
2019-01-14 13:47:19 +0700

    Add private policy

--- src/pages/main/components/login-router/login-form/index.js
+++ src/pages/main/components/login-router/login-form/index.js
@@ -9,8 +9,8 @@ import { withRouter } from 'react-router';
 
 const model = {
     tree: {
-        username: '',
-        password: '',
+        username: 'doctor@yandex.ru',
+        password: '1',
     },
 };
 
@@ -89,6 +89,12 @@ export const LoginForm = schema(model)(withRouter(React.createClass({
                     Submit
                 </Button>
                 <FormErrorMessages errorTexts={errorTexts} />
+                <div style={{ margin: '10px 0' }}>
+                    <a href="https://www.freeprivacypolicy.com/privacy/view/d2a3a4d6658dd04af75792af4676e91f"
+                       target="_blank">
+                        Private policy
+                    </a>
+                </div>
             </Form>
         );
     },
